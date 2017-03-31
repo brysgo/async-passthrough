805d33135a4fc6328005f8b2cd8532ca1b21f14c
Bryan Goldstein <brysgo@gmail.com>
2017-03-31 00:13:10 -0400

    Add promise implementation (from graphql-anywhere)

--- promise.js
+++ promise.js
@@ -1,5 +1,21 @@
-function detect(obj) {
-  return obj && typeof obj.then === "function";
+function deferrableOrImmediate(obj, fn) {
+  if (detect(obj)) {
+    return obj.then(fn);
+  } else {
+    return fn(obj);
+  }
 }
 
-module.exports = { detect };
+function arrayOrDeferrable(arr) {
+  if (arr.some(detect)) {
+    return Promise.all(arr);
+  } else {
+    return arr;
+  }
+}
+
+module.exports = { deferrableOrImmediate, arrayOrDeferrable };
+
+function detect(obj) {
+  return obj && typeof obj === "object" && typeof obj.then === "function";
+}
